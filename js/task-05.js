178c06573714359adba208d26ba0e9882d84ce71
Nadezhda Kovalenko <nadina00@ukr.net>
2022-05-08 21:52:24 +0300

    изменяет 5 задачу

--- js/task-05.js
+++ js/task-05.js
@@ -3,15 +3,17 @@ const outputEl = document.querySelector("#name-output");
 console.log(outputEl);
 inputEl.addEventListener("focus", onInputFocus)
 function onInputFocus(){
-    console.log(`input on focus`);
+    
 }
-inputEl.addEventListener("input", event => {
-             
-    outputEl.textContent = event.currentTarget.value; 
+inputEl.addEventListener("input", event => {      
+outputEl.textContent = event.currentTarget.value; 
     
 })
 inputEl.addEventListener("blur",onInputBlur);
-function onInputBlur(){
-   
+function onInputBlur(event){
+    if(event.currentTarget.value === ""){
+        outputEl.textContent = "Anonymous";
+         
+     }    
 }
 console.log(inputEl.value);
